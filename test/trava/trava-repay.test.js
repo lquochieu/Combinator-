017e86ae7a4c8a14fcd250b132f7f496c87feb1c
ngocban284 <daongocban284@gmail.com>
2023-06-28 11:07:08 +0700

    repay test

--- test/trava/trava-repay.test.js
+++ test/trava/trava-repay.test.js
@@ -1,56 +1,59 @@
-const hre = require('hardhat');
+const hre = require("hardhat");
 const abiCoder = new hre.ethers.utils.AbiCoder();
 require("dotenv").config();
 
-const { Action } = require('../teststrategy/Action');
+const { Action } = require("../teststrategy/Action");
+const { getProxy } = require("../utils");
 
-describe('Trava-Repay', function () {
-    this.timeout(150000);
+describe("Trava-Repay", function () {
+  this.timeout(150000);
 
-    it("Test trava repay", async() => {
-        
-        const market = "0x6df52f798740504c24ccd374cf7ce81b28ce8330"
-        const tokenAddress = process.env.WBNB_BSCTESTNET
-        const amount = 1e12
-        const from = process.env.PUCLIC_KEY
-        const onBehalf = process.env.DS_PROXY
-        
-        console.log("prepare repay")
-       
-        const traveRepay = new Action("TravaRepay", process.env.TRAVA_REPAY, ['address', 'address', 'uint256', 'address', 'address'], [market, tokenAddress, amount, from, onBehalf, enableAsColl])
-        
-        const calldata = traveRepay.encodeForRecipe()[0]
-        //console.log("calldata", calldata)
+  it("Test trava repay", async () => {
+    const market = "0x6df52f798740504c24ccd374cf7ce81b28ce8330";
+    const tokenAddress = process.env.WBNB_BSCTESTNET;
+    const amount = 1e14;
+    const from = process.env.PUBLIC_KEY;
+    const proxy = await getProxy(process.env.PUBLIC_KEY);
+    const onBehalf = proxy.address;
+    const enableAsColl = false;
+    const trava = "0xE1F005623934D3D8C724EC68Cc9bFD95498D4435";
+    const tTrava = "0x299B7c0d944DB2BA1C017fEF25B630b001e06968";
+    const IERC20 = await hre.ethers.getContractAt("IERC20Test", trava);
+    await IERC20.approve(proxy.address, amount);
 
-        const subdata = [
-            abiCoder.encode(['address'], [market]),
-            abiCoder.encode(['address'], [tokenAddress.toString()]),
-            abiCoder.encode(['uint256'], [amount.toString()]),
-            abiCoder.encode(['address'], [from.toString()]),
-            abiCoder.encode(['address'], [onBehalf.toString()]),
-            abiCoder.encode(['bool'], [enableAsColl])
-        ]
-        //console.log("subdata", subdata)
+    console.log("prepare repay");
 
-        const parramMapping = traveRepay.encodeForRecipe()[3]
-        //console.log("parramMapping", parramMapping)
+    const traveRepay = new Action(
+      "TravaRepay",
+      process.env.TRAVA_REPAY_ADDRESS,
+      ["address", "address", "uint256", "address", "address"],
+      [market, trava, amount, from, onBehalf]
+    );
 
-        const returnValues = "0x0000000000000000000000000000000000000000000000000000000000000000"
-        //console.log("returnValues", returnValues)
+    const calldata = traveRepay.encodeForDsProxyCall()[1];
+    console.log("calldata", calldata);
 
-        const repayInput = {
-            calldata: calldata,
-            subdata: subdata,
-            parramMapping: [parramMapping],
-            returnValues: returnValues
-        }
+    const repayContract = await hre.ethers.getContractAt(
+      "TravaRepay",
+      process.env.TRAVA_REPAY_ADDRESS
+    );
 
-        console.log(repayInput)
-        // const repayContract = await hre.ethers.getContractAt("TravaRepay", process.env.TRAVA_SUPPLY)
-        // console.log("start Repay")
-        // // const travaRepay = await repayContract.executeAction(calldata, subdata, [parramMapping], [returnValues]);
-        // const travaRepay = await repayContract.executeActionDirect(calldata);
+    // call receive function in proxy contract to send BNB to proxy
+    // const ownerAcc = (await hre.ethers.getSigners())[0];
+    // await ownerAcc.sendTransaction({
+    //   to: proxy.address,
+    //   value: amount + amount,
+    // });
 
-//        console.log("ok")
-    })
+    let tx = await proxy["execute(address,bytes)"](
+      repayContract.address,
+      calldata,
+      {
+        gasLimit: 20000000,
+      }
+    );
+
+    tx = await tx.wait();
+    console.log("tx", tx);
+  });
 });
