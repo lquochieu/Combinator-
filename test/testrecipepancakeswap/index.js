14c968d65728ce641a60f1156278c28c8632279e
Ryan.Nguyen <Ryan.Nguyen@avepoint.com>
2023-07-05 16:48:48 +0700

    x

--- test/testrecipepancakeswap/index.js
+++ test/testrecipepancakeswap/index.js
@@ -1,6 +1,6 @@
 // Tạo 2 token -> tạo 1 pool vói 2 token -> Test action 1 là addLiquidity cả 2 token đó -> Test action 2 là increaseLiquidity 1 đồng -> Test action 3 là swap -> Test action 4 là collect lãi do 1 thằng khác đã swap ở bước trước > Test aciton 5 là removeLiquidity
 
-const { getProxy, addLiquidity, increaseLiquidity, createPool } = require("./utils");
+const { getProxy, addLiquidity, increaseLiquidity, createPool, collectLiquidity, swapTokenForToken, removeLiquidity } = require("./utils");
 
 // Account Owner support 2 token
 // Account A swap
@@ -8,7 +8,6 @@ const { getProxy, addLiquidity, increaseLiquidity, createPool } = require("./uti
 describe("Test Pancakeswap", async function() {
   let ownerAcc;
   let accA;
-  let accB;
   let proxy;
 
   before(async() => {
@@ -41,8 +40,8 @@ describe("Test Pancakeswap", async function() {
     // Send token to SW 
     // await tokenA.transfer(proxy.address, "11000000000000000000");
     // await tokenB.transfer(proxy.address, "11000000000000000000");
-    console.log("Balance token A of Proxy::", await tokenA.balanceOf(proxy.address));
-    console.log("Balance token B of Proxy::", await tokenB.balanceOf(proxy.address));
+    // console.log("Balance token A of Proxy::", await tokenA.balanceOf(proxy.address));
+    // console.log("Balance token B of Proxy::", await tokenB.balanceOf(proxy.address));
   });
 
   it("Test addLiquidity", async() => {
@@ -66,25 +65,27 @@ describe("Test Pancakeswap", async function() {
     // );
   });
   it("Test createPool", async() => {
-    // Lỗi
-    await createPool(
-      ownerAcc,
-      proxy, 
-      process.env.TOKEN_B_TEST2, 
-      process.env.TOKEN_A_TEST2, 
-      "2500", 
-      "-28150", 
-      "-26050", 
-      "145075268333520540069",
-      "10000000000000000000",
-      "0", 
-      "0", 
-      "0x595622cbd0fc4727df476a1172ada30a9ddf8f43", 
-      "2688452425",
-      "0x595622cbd0fc4727df476a1172ada30a9ddf8f43",
-      '20456476331960289157024907122'
-    );
-  })
+    // // Phải tạo lại 2 token mới và thế vào env nhé
+    // // Action này createPool r cung vào pool luôn
+    // // Lỗi lạ xuất hiện là do thiếu gasLimit
+    // await createPool(
+    //   ownerAcc,
+    //   proxy, 
+    //   process.env.TOKEN_B_TEST2, 
+    //   process.env.TOKEN_A_TEST2, 
+    //   "2500", 
+    //   "-28150", 
+    //   "-26050", 
+    //   "145075268333520540069",
+    //   "10000000000000000000",
+    //   "0", 
+    //   "0", 
+    //   "0x595622cbd0fc4727df476a1172ada30a9ddf8f43", 
+    //   "2688452425",
+    //   "0x595622cbd0fc4727df476a1172ada30a9ddf8f43",
+    //   '20456476331960289157024907122'
+    // );
+  });
   it("Test increaseLiquidity", async() => {
     // await increaseLiquidity(
     //   proxy, 
@@ -98,5 +99,33 @@ describe("Test Pancakeswap", async function() {
     //   "2688452425", 
     //   "0x595622cbd0fc4727df476a1172ada30a9ddf8f43"
     // );
-  })
+  });
+  it("Test Collect", async() => {
+    // // Chú ý phải approve cho proxy.address được phép dùng Token bằng INonFungiblePositionManager.approve(proxyaddress, 2993) => ta đã làm nó trong bscscan(https://testnet.bscscan.com/address/0x427bF5b37357632377eCbEC9de3626C71A5396c1#writeContract), phải code ra. Bản thân nó là ERC721
+    // await collectLiquidity(
+    //   ownerAcc, proxy, "2993", "0x595622cbd0fc4727df476a1172ada30a9ddf8f43", 100000000000000, 100000000000000
+    // );
+  });
+  it("Test Remove", async() => {
+    // // Chú ý phải approve cho proxy.address được phép dùng Token bằng INonFungiblePositionManager.approve(proxyaddress, 2993) => ta đã làm nó trong bscscan(https://testnet.bscscan.com/address/0x427bF5b37357632377eCbEC9de3626C71A5396c1#writeContract), phải code ra. Bản thân nó là ERC721
+    // await removeLiquidity(
+    //   ownerAcc, proxy, "2994", "1000", 0, 0, 2688452425
+    // );
+  });
+  it("Test Swap", async() => {
+    // Đầu tiên gửi TOKEN_A_TEST2 cho accA cho nó có đủ tiền
+    const tokenA = (await hre.ethers.getContractAt("ERC20Mock", process.env.TOKEN_A_TEST2)).connect(ownerAcc);
+    // await tokenA.transfer(accA.address, "1000000000000000000");
+    console.log("Balance token A of accA::", await tokenA.balanceOf(accA.address));
+
+    // Lấy proxy của accA để thực hiện thông qua proxy
+    const proxyA = (await getProxy(accA.address)).connect(accA);
+    console.log(`Proxy address:: ${proxyA.address} with balance ${hre.ethers.utils.formatEther(await ethers.provider.getBalance(proxyA.address))} TBNB`);
+
+    // Phải approve cho router contract => contract đã có rồi
+    
+    await swapTokenForToken(
+      accA, proxyA, "10000000000", 0, ["0xfFa52085f3a06AD915EDdC70Fe5564F28852A0e0", "0x5FA12BE425b51da9bcd4C835a341E2A3A31dC214"], accA.address, accA.address
+    );
+  });
 });
--- test/testrecipepancakeswap/utils.js
+++ test/testrecipepancakeswap/utils.js
@@ -139,9 +139,75 @@ const createPool = async (owner, proxy, token0, token1, fee, tickLower, tickUppe
   const position = await hre.ethers.getContractAt("INonfungiblePositionManager",process.env.NON_FUNGIBLE_POSITION_MANAGER);
   const length = await position.balanceOf(owner.address);
   console.log("Before, balance of user::", length);
-  // for(var i = 0; i < length; i++){
-  //   console.log("::", await position.tokenOfOwnerByIndex(owner.address, i));
-  // }
+  for(var i = 0; i < length; i++){
+    console.log("::", await position.tokenOfOwnerByIndex(owner.address, i));
+  }
+
+  let tx = await proxy["execute(address,bytes)"](
+    createPoolContract,
+    functionData, {
+      gasLimit: 10000000, // éo hiểu sao bỏ cái này đi gây lỗi
+    }
+  );
+  tx = await tx.wait();
+  console.log("tx hash::", tx.transactionHash);
+
+  const length2 = await position.balanceOf(owner.address);
+  console.log("After, balance of user::", length2);
+  console.log("Last index is::", await position.tokenOfOwnerByIndex(owner.address, length2 - 1));
+}
+
+const collectLiquidity = async (owner, proxy, tokenId, recipient, amount0Max, amount1Max) => {
+  console.log("Run to collectLiquidity");
+  
+  const createPoolAction = new Action(
+    "PancakeCollectV3",
+    process.env.PANCAKE_COLLECT_V3_ADDRESS,
+    ["uint256", "address", "uint128", "uint128"],
+    [tokenId, recipient, amount0Max, amount1Max]
+  );
+  console.log(tokenId, recipient, amount0Max, amount1Max);
+  const functionData = createPoolAction.encodeForDsProxyCall()[1];
+
+  const createPoolContract = await getAddrFromRegistry("PancakeCollectV3");
+  console.log(createPoolContract);
+
+  const position = await hre.ethers.getContractAt("INonfungiblePositionManager",process.env.NON_FUNGIBLE_POSITION_MANAGER);
+  const length = await position.positions(tokenId);
+  console.log("Before, position of token::", length);
+
+  let tx = await proxy["execute(address,bytes)"](
+    createPoolContract,
+    functionData, {
+      gasLimit: 10000000,
+    }
+  );
+  tx = await tx.wait();
+  console.log("tx hash::", tx.transactionHash);
+
+  const length2 = await position.positions(tokenId);
+  console.log("After, position of token::", length2);
+}
+
+const swapTokenForToken = async (accA, proxy, amountIn, amountOutMin, path, to, from) => {
+  console.log("Run to swapTokenForToken");
+
+  const createPoolAction = new Action(
+    "PancakeSwapV3",
+    process.env.PANCAKE_SWAP_V3_ADDRESS,
+    ["uint256", "uint256", "address[]", "address", "address"],
+    [amountIn, amountOutMin, path, to, from]
+  );
+
+  console.log(amountIn, amountOutMin, path, to, from);
+  const functionData = createPoolAction.encodeForDsProxyCall()[1];
+
+  const createPoolContract = await getAddrFromRegistry("PancakeSwapV3");
+  console.log(createPoolContract);
+
+  const tokenB = (await hre.ethers.getContractAt("ERC20Mock", process.env.TOKEN_B_TEST2));
+  // await tokenA.transfer(accA.address, "1000000000000000000");
+  console.log("Before, balance token B of accA::", await tokenB.balanceOf(accA.address));
 
   let tx = await proxy["execute(address,bytes)"](
     createPoolContract,
@@ -150,14 +216,47 @@ const createPool = async (owner, proxy, token0, token1, fee, tickLower, tickUppe
   tx = await tx.wait();
   console.log("tx hash::", tx.transactionHash);
 
-  const length2 = await position.balanceOf(owner.address);
-  console.log("After, balance of user::", length2);
-  // console.log("Last index is::", await position.tokenOfOwnerByIndex(owner.address, length2 - 1));
+  console.log("After, balance token B of accA::", await tokenB.balanceOf(accA.address));
+}
+
+const removeLiquidity = async (owner, proxy, tokenId, liquidity, amount0Min, amount1Min, deadline) => {
+  console.log("Run to removeLiquidity");
+  const createPoolAction = new Action(
+    "PancakeRemoveLiquidityV3",
+    process.env.PANCAKE_REMOVE_LIQUIDITY_V3_ADDRESS,
+    ["uint256", "uint128", "uint256", "uint256", "uint256"],
+    [tokenId, liquidity, amount0Min, amount1Min, deadline]
+  );
+
+  console.log(tokenId, liquidity, amount0Min, amount1Min, deadline);
+  const functionData = createPoolAction.encodeForDsProxyCall()[1];
+
+  const createPoolContract = await getAddrFromRegistry("PancakeRemoveLiquidityV3");
+  console.log(createPoolContract);
+
+  const position = await hre.ethers.getContractAt("INonfungiblePositionManager",process.env.NON_FUNGIBLE_POSITION_MANAGER);
+  const length = await position.positions(tokenId);
+  console.log("Before, position of token::", length);
+
+  let tx = await proxy["execute(address,bytes)"](
+    createPoolContract,
+    functionData, {
+      gasLimit: 10000000,
+    }
+  );
+  tx = await tx.wait();
+  console.log("tx hash::", tx.transactionHash);
+
+  const length2 = await position.positions(tokenId);
+  console.log("After, position of token::", length2);
 }
 
 module.exports = {
   getProxy,
   addLiquidity,
   increaseLiquidity,
-  createPool
+  createPool,
+  collectLiquidity,
+  swapTokenForToken,
+  removeLiquidity
 }
